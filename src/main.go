92551ed7f4058034b5258b56ff09c8ff00944430
vperked <perked@riseup.net>
2024-04-19 20:55:29 -0400

    Added some baller stuff.

--- src/main.go
+++ src/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func main() {
-	for i := 0; i < 5; i++ {
+	for i := 0; i < 100; i++ {
 		get()
 	}
+}
+
+type Message struct {
+	Content string `json:"content"`
 }
 
 func get() {
@@ -19,11 +25,31 @@ func get() {
 	}
 	website := "https://vperked.online"
 	resp, err := client.Get(website)
-	resp.Header.Add("User-Agent", "Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36")
+	resp.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.3")
 	if err != nil {
 		fmt.Println("Could not send the GET Request = ", err)
 		return
 	}
 	defer resp.Body.Close()
 	fmt.Println("Spamming and it is responding with: ", resp.Status)
+
+	message := Message{
+		Content: resp.Status,
+	}
+
+	messageJSON, err := json.Marshal(message)
+	if err != nil {
+		fmt.Println("Couldnt marshal message! ", err)
+		return
+	}
+
+	webhookURL := "https://discord.com/api/webhooks/1231044284464234537/X0U2CoW7jwhnaqrUCiifahbVV9jTm5CAILsyVdP394HXraE5RklbBfVwfmDPHqoaqskz"
+
+	webhookResp, err := http.Post(webhookURL, "application/json", bytes.NewBuffer(messageJSON))
+	if err != nil {
+		fmt.Println("Couldnt send post request!", err)
+		return
+	}
+	defer webhookResp.Body.Close()
+	fmt.Println("Sent message")
 }
